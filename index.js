50fe3748cdf52e0404f642fb4e7a76857937681b
Jiani Liu <liujiani505@gmail.com>
2023-04-06 14:54:22 -0400

    calling api

--- index.js
+++ index.js
@@ -7,6 +7,24 @@ let result = document.getElementById("result");
 let getMovie = () => {
     let movieName = movieNameRef.value;
     let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
-
+    // if input field is empty
+    if(movieName.length <=0){
+        result.innerHTML = `<h3 class="msg">Please enter a movie name.</h3>`
+    }
+    // if input isn't empty
+    else {
+        fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            // if movie exist in database
+            if(data.Response == "Ture"){
+                result.innerHTML = `
+                    <div class="info">
+                        <img src=${data.Poster} class="poster">
+                    </div>
+                `
+            }
+        })
+    }
 }
 
