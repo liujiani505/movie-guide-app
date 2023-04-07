12cecb93aa360a8580271aef1bb4622db08320e5
jianiliu <liujiani505@gmail.com>
2023-04-07 11:06:41 -0400

    display fetched api data

--- index.js
+++ index.js
@@ -16,15 +16,42 @@ let getMovie = () => {
         fetch(url)
         .then((response) => response.json())
         .then((data) => {
+            console.log(data)
             // if movie exist in database
-            if(data.Response == "Ture"){
+            if(data.Response == "True"){
                 result.innerHTML = `
                     <div class="info">
                         <img src=${data.Poster} class="poster">
+                        <div>
+                            <h2>${data.Title}</h2>
+                            <div class="rating">
+                                <img src="star-icon.svg">
+                                <h4>${data.imdbRating}</h4>
+                            </div>
+                            <div class="details">
+                                <span>${data.Rated}</span>
+                                <span>${data.Year}</span>
+                                <span>${data.Runtime}</span>
+                            </div>
+                            <div class="genre">
+                                <div>${data.Genre}</div>
+                            </div>
+                        </div>
                     </div>
-                `
+                    <h3>Plot:</h3>
+                    <p>${data.Plot}</p>
+                    <h3>Cast:</h3>
+                    <p>${data.Actors}</p>
+                `;
+            }
+            // if movie doesn't exist in database
+            else {
+                result.innerHTML = `<h3 class="msg">${data.Error}</h3>`
             }
         })
+        .catch(() => {
+            result.innerHTML = `<h3 class="msg">Error Occured</h3>`;
+        });
     }
-}
+};
 
